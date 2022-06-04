ee33febc677d0fb6a24e02533af4b06a16930b17
SMatakane <matakane558@gmail.com>
2022-06-04 02:57:22 -0700

    low-level

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Test sign
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 'a';
+
+	for (; x <= 'z'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
