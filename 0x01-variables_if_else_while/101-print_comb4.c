6a75228c4c7acac0d3cbec9ee78462b6f8db8f82
SMatakane <matakane558@gmail.com>
2022-06-04 05:28:10 -0700

    low-level

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Test sign
+ * Return: 0
+ */
+int main(void)
+{
+	int x = '0';
+	int y = '1';
+	int z = '2';
+
+	for (; x <= '7'; x++)
+	{
+		int temp_y = y;
+		int temp_z = z;
+
+		for (; temp_y <= '8'; temp_y++)
+		{
+			int temp = temp_z;
+
+			for (; temp <= '9'; temp++)
+			{
+				putchar(x);
+				putchar(temp_y);
+				putchar(temp);
+				if (x != '7' && temp_y != '8' && temp != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			temp_z++;
+		}
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
