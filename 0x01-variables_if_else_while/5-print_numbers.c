8e32b8ad10853f031165063bc36fef0c67da2935
SMatakane <matakane558@gmail.com>
2022-06-04 03:16:30 -0700

    low-level

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Test sign
+ * Return: 0
+ */
+int main(void)
+{
+	int x = '0';
+
+	for (; x <= '9'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
