6cc2888c0d630e7aafc082f0fc1fd2c4d38e992c
SMatakane <matakane558@gmail.com>
2022-06-07 07:45:06 -0700

    low-level

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,26 +8,18 @@
  */
 void print_to_98(int n)
 {
-	while (n < 98)
+	int i, j;
+
+	if (n > 98)
 	{
-		printf("%d", n);
-		if (n != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		n++;
+		for (; n > 98; n--)
+			printf("%d, ", n);
 	}
-	while (n > 98)
+	else if (n < 98)
 	{
-		printf("%d", n);
-		if (n != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		n--;
+		for (; n < 98; n++)
+			printf("%d, ", n);
 	}
-	if (n == 98)
+	else
 		printf("98\n");
 }
