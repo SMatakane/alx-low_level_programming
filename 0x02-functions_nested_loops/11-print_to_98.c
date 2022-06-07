10785b2fd0cf81ce983bff11e83adcc66fffa25c
SMatakane <matakane558@gmail.com>
2022-06-07 07:50:57 -0700

    low-level

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,13 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (; n > 96; n--)
+		for (; n > 97; n--)
 			printf("%d, ", n);
 	}
 	else if (n < 98)
 	{
-		for (; n < 98; n++)
+		for (; n < 99; n++)
 			printf("%d, ", n);
 	}
-	else
-		printf("98\n");
+	printf("98\n");
 }
