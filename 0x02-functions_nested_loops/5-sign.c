1c86bbc1d9039e6cff0916d9f0bbfafd1bcda106
SMatakane <matakane558@gmail.com>
2022-06-07 06:12:01 -0700

    low-level

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,44 +1,6 @@
 #include <stdio.h>
 
 /**
- * print_sign - prototype
- * @n: param
- * Return: sign
- */
-int print_sign(int n);
-
-/**
- * main - main
- * Return: 0
- */
-int main(void)
-{
-	int r;
-
-	r = print_sign(98);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(0);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(0xff);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(-1);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
-}
-
-/**
  * print_sign - main
  * @n: value to test
  * Return: -1 if neg 0 if 0 and 1 if positive
