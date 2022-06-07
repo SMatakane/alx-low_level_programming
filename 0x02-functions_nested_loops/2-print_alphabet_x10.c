81c28ea3a002ad3cb5ae1efdc9719fd1f0d3f19d
SMatakane <matakane558@gmail.com>
2022-06-07 05:11:58 -0700

    low-level

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,38 +1,4 @@
-#include <stdio.h>
-
-/**
- * print_alphabet - prints
- * Return: 0
- */
-void print_alphabet(void);
-
-/**
- * print_alphabet_x10 - times 10
- * Return: (0)
- */
-void print_alphabet_x10(void);
-/**
- * main - main
- * Return: 0
- */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
-/**
- * print_alphabet - none
- * Return: 0
- */
-void print_alphabet(void)
-{
-	int i = 'a';
-
-	for (; i <= 'z'; i++)
-		putchar(i);
-	putchar('\n');
-}
+#include "main.h"
 
 /**
  * print_alphabet_x10 - times 10
@@ -43,5 +9,13 @@ void print_alphabet_x10(void)
 	int i = 0;
 
 	for (; i < 10; i++)
-		print_alphabet();
+	{
+		char j = 'a';
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+	}
 }
