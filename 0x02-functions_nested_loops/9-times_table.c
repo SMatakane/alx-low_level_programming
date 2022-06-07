f4e79fee57df7edac8f1922d08f452f79606cf9e
SMatakane <matakane558@gmail.com>
2022-06-07 06:54:43 -0700

    low-level

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - table of tables
+ * Return: 0
+ */
+void times_table(void)
+{
+	int row = 0;
+	int val, hundred, ten, unit = 0;
+
+	for (; row <= 12; row++)
+	{
+		int col = 0;
+		for (; col <= 12; col++)
+		{
+			val = row * col;
+			hundred = (val/ 100) % 10;
+			ten = (val / 10) % 10;
+			unit = (val / 1) % 10;
+
+			if (hundred != 0)
+				_putchar('0' + hundred);
+			else
+				_putchar(' ');
+			if (ten != 0 || (hundred != 0 && ten == 0))
+				_putchar('0' + ten);
+			else
+				_putchar(' ');
+			_putchar('0' + unit);
+			if (col != 12)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+				_putchar('\n');
+		}
+	}
+}
