b74a5e862b995efcc316b11db072abdda9781c24
SMatakane <matakane558@gmail.com>
2022-06-07 08:07:59 -0700

    low-level

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,12 +30,13 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(' ');
 				}
 			}
 			_putchar('\n');
 		}
 	}
-	else
+	else if (n == 0)
 	{
 		_putchar('0');
 		_putchar('\n');
