3b88f398a2b11ec098e6383782f306f26fa2858f
SMatakane <matakane558@gmail.com>
2022-06-07 06:16:51 -0700

    low-level

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,27 +1,4 @@
-#include <stdio.h>
-
-/**
- * print_last_digit - prototype
- * @val: value
- * Return: last digit
- */
-int print_last_digit(int val);
-
-/**
- * main - main
- * Return: 0
- */
-int main(void)
-{
-	int r;
-
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	putchar('0' + r);
-	putchar('\n');
-	return (0);
-}
+#include "main.h"
 
 /**
  * print_last_digit - prints last digit
@@ -33,6 +10,6 @@ int print_last_digit(int val)
 	if (val < 0)
 		val *= -1;
 	val %= 10;
-	putchar('0' + val);
+	_putchar('0' + val);
 	return (val);
 }
