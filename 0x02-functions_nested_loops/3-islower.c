9bd88b149f371bfd095fbdd2ab817a4220b843fc
SMatakane <matakane558@gmail.com>
2022-06-07 05:24:53 -0700

    low-level

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,31 +1,6 @@
 #include <stdio.h>
 
 /**
- * _islower - lower case check
- * @c: character
- * Return: bool
- */
-int _islower(int c);
-
-/**
- * main - main
- * Return: 0
- */
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	putchar(r + '0');
-	r = _islower('o');
-	putchar(r + '0');
-	r = _islower(108);
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
-}
-
-/**
  * _islower - main
  * @c: char
  * Return: 0 or 1
