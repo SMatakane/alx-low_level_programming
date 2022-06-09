1acdc75556aede4c053f4bc289542989426e43bd
SMatakane <matakane558@gmail.com>
2022-06-09 09:51:50 -0700

    low-level

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_line - prints line
+ * print_diagonal - prints line
  * @n: length of line
  * Return: 0
  */
 void print_diagonal(int n)
 {
 	int i = 0;
-	
+
 	_putchar('\n');
 	for (; i < n; i++)
 	{
 		int j = 0;
-		
+
 		for (; j < i; j++)
 			_putchar(' ');
 		_putchar('\\');
