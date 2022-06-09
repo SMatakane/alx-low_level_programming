3cfc4cb9d512087325c07187da43de8b11875f91
SMatakane <matakane558@gmail.com>
2022-06-09 10:09:59 -0700

    low-level

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_square - prints squares
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i = 0;
+	for (; i < size; i++)
+	{
+		for (; j < size; j++)
+			_putchar('#');
+		_putchar('\n')
+	}
+}
