ebd982d8127ca82dbe80443b5b32c9d424364281
SMatakane <matakane558@gmail.com>
2022-06-09 10:17:38 -0700

    low-level

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(void)
+{
+	print_square(0);
+	print_square(2);
+	print_square(8);
+	print_square(-1);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,16 +2,20 @@
 
 /**
  * print_square - prints squares
+ * @size: size
  * Return: void
  */
 void print_square(int size)
 {
 	int i = 0;
+
 	for (; i < size; i++)
 	{
+		int j = 0;
+
 		for (; j < size; j++)
 			_putchar('#');
-		_putchar('\n')
+		_putchar('\n');
 	}
 	if (size <= 0)
 		_putchar('\n');
