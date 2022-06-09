f261cfd4227720d6f130546c6bee331e575becb9
SMatakane <matakane558@gmail.com>
2022-06-09 09:49:57 -0700

    low-level

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,17 +8,15 @@
 void print_diagonal(int n)
 {
 	int i = 0;
-
-	for (; i <= n; i++)
+	
+	_putchar('\n');
+	for (; i < n; i++)
 	{
 		int j = 0;
-		if (i < n)
-		{
-			for (; j < i; j++)
-				_putchar(' ');
-			_putchar('\\');
-		}
-		if (i == 0)
-			_putchar('\n');
+		
+		for (; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
