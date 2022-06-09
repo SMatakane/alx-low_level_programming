498644907bd9c7badf52f759a46916d28faad9d2
SMatakane <matakane558@gmail.com>
2022-06-09 00:30:19 -0700

    low-level

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ * @n: length of line
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
