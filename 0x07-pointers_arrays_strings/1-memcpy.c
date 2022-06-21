a96e0ed4409eff3c9a53df24ed2572c9a72371fa
SMatakane <matakane558@gmail.com>
2022-06-21 09:37:34 -0700

    low-level

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy: copies mem to dest
+ * @dest: destination
+ * @src: source
+ * @n: bytes to reserve
+ * Return: char val
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+		dest[i] = *(src + i);
+	return (dest);
+}
