05429c41268695cbc38bd278a20f7b1dd82afa1f
SMatakane <matakane558@gmail.com>
2022-06-22 01:58:11 -0700

    low-level

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,33 @@
 #include <math.h>
 
 /**
+ * _eval - does the mathy stuff
+ * @i: input
+ * @n: input
+ * Return: evaluation
+ */
+int _eval(int i, int n)
+{
+	if (n == 0 || n == 1)
+		return (n);
+	else if (i * i < n)
+		return (_eval(i + 1, n));
+	else if (i * i == n)
+		return (i);
+	return (-1);
+}
+
+/**
  * _sqrt_recursion - recursion of sqrt, later implement newton method
  * @n: input
  * Return: integer out
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
+	int i = 0;
+
+	if (i < 0)
 		return (-1);
-	else if (n == 0 || n == 1)
-		return (n);
 	else
-		return (sqrt(n - 1));
+		return (_eval(i, n));
 }
