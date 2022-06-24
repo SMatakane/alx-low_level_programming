b70d2289c41d60653e9bd5ccb8786f26f223a28e
SMatakane <matakane558@gmail.com>
2022-06-24 10:15:06 -0700

    low-level

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - main
+ * @argc: integer
+ * @argv: array
+ * Return (0)
+ */
 int main(int argc, char *argv[])
 {
+	argc = argc;
 	printf("%s\n", *argv);
 	return (0);
 }
