73e21963179290254ce57a7c6ec7aba91c25f602
SMatakane <matakane558@gmail.com>
2022-06-27 09:42:17 -0700

    low-level

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <main.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - something
+ * @str: string input
+ * Return: pointer to something
+ */
+char *_strdup(char *str)
+{
+	char *A;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	if ( str == NULL)
+		return (NULL);
+	for (; str[i] != '\0'; i++)
+	{
+	}
+
+	A = (char *)malloc(sizeof(char) * (i + 1));
+	if (A == NULL)
+		return (NULL);
+	for (; j <= i; j++)
+		A[j] = str[j];
+	return (A);
+}
