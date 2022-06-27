240b99cecf73c058a0250d8ca85aa7150746cb82
SMatakane <matakane558@gmail.com>
2022-06-27 09:28:06 -0700

    low-level

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+char *create_array(unsigned int size, char c)
+{
+	char *c_;
+	unsigned int i;
+
+	if (size == 0)
+		return NULL;
+	c_ = malloc(sizeof(c) * size);
+
+	if (c_ == NULL)
+		return NULL;
+	for (; i < size; i++)
+		c_[i] = c;
+	return (c_);
+}
