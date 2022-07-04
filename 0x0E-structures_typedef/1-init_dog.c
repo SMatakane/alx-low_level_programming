27fb83096499c4ef707c3d5c8015e84302a45baf
SMatakane <matakane558@gmail.com>
2022-07-04 00:24:22 -0700

    low-level

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - fetch
+ * d: dmx would be proud
+ * name: chptr
+ * age: in human years lol
+ * owner: snoop
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
