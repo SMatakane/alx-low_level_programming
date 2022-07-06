6e193a06974a44a5a2b6fe6a166e8181777c7cb7
SMatakane <matakane558@gmail.com>
2022-07-06 03:48:26 -0700

    low-level

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - adds
+ * @a: meh
+ * @b: meh
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subs
+ * @a: meh
+ * @b: meh
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - mul
+ * @a: meh
+ * @b: meh
+ * Return: mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - div
+ * @a: meh
+ * @b: meh
+ * Return: dib
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mod
+ * @a: meh
+ * @b: meh
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
