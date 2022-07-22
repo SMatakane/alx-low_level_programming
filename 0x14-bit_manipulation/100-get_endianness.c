fd7cc966c1f289ad61536afa0d8eb2defe81795d
SMatakane <matakane558@gmail.com>
2022-07-22 01:33:12 -0700

    low-level

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_endianness - little horsie
+ * Return: int
+ */
+int get_endianness(void)
+{
+	int num = 1;
+	char *endian = (char *) &num;
+
+	if (*endian == 1)
+		return (1);
+	return (0);
+}
