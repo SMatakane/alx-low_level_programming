47ac36ab6332b262b4290f7a2c277018ead024fc
SMatakane <matakane558@gmail.com>
2022-07-22 01:08:56 -0700

    low-level

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+/**
+ * binary_to_uint - as the name says
+ * @b: val
+ * Return: as uint
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec;
+	unsigned int i;
+
+	for (dec = 0, i = 0; b[i] !='\0'; i++)
+	{
+		if (b[i] == '1')
+			dec = (dec << 1) | 1;
+		else if (b[i] == '0')
+			dec <<= 1;
+		else if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	return (dec);
+}
