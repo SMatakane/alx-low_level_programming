cc96c6664effbaa4ad00dbbb61f4e0b070fbb6a1
SMatakane <matakane558@gmail.com>
2022-09-01 06:43:22 -0700

    high-level

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - as the name implies
+ * @head: head
+ * @index: idx
+ * Return: addr/null
+ */
+dlistint_t *get_dnoeint_at_index(dlistint_t *head, unsigned int index)
+{
+	for (; index != 0; index--)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
+}
